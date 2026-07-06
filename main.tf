1bbc9ffb70a8a85157c0d4216985af41649aa574
Kabi <kawasthi098@gmail.com>
2026-07-06 08:50:32 +0545

    force delete for ecr repo

--- main.tf
+++ main.tf
@@ -55,6 +55,8 @@ data "aws_security_group" "ecs_sg" {
 resource "aws_ecr_repository" "app" {
 
   name = var.app_name
+  force_delete = true
+
 
   image_scanning_configuration {
     scan_on_push = true
